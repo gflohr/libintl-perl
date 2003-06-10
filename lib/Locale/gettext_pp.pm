912014db206b9746c1b75c72dd72c51cb855825f
Guido Flohr <guido.flohr@cantanea.com>
2003-06-10 16:06:39 +0000

    moved version information to Locale::TextDomain

--- lib/Locale/gettext_pp.pm
+++ lib/Locale/gettext_pp.pm
@@ -1,7 +1,7 @@
 #! /bin/false
 
 # vim: tabstop=4
-# $Id: gettext_pp.pm,v 1.4 2003/06/04 12:16:44 guido Exp $
+# $Id: gettext_pp.pm,v 1.5 2003/06/10 16:06:39 guido Exp $
 
 # Pure Perl implementation of Uniforum message translation.
 # Copyright (C) 2002-2003 Guido Flohr <guido@imperia.net>,
@@ -34,10 +34,6 @@ package Locale::gettext_pp;
 use strict;
 
 require 5.004;
-
-use vars qw ($VERSION);
-
-$VERSION = '0.07';
 
 use vars qw ($__gettext_pp_default_dir 
 			 $__gettext_pp_textdomain
