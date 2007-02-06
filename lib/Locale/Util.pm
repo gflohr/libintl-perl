96c2da2c1689f186f314884dcf36f7f977741c8e
Guido Flohr <guido.flohr@cantanea.com>
2007-02-06 16:14:45 +0000

    Support for MS-DOS

--- lib/Locale/Util.pm
+++ lib/Locale/Util.pm
@@ -1,7 +1,7 @@
 #! /bin/false
 
 # vim: set autoindent shiftwidth=4 tabstop=4:
-# $Id: Util.pm,v 1.3 2007/02/06 13:41:26 guido Exp $
+# $Id: Util.pm,v 1.4 2007/02/06 16:14:45 guido Exp $
 
 # Portable methods for locale handling.
 # Copyright (C) 2002-2007 Guido Flohr <guido@imperia.net>,
@@ -134,7 +134,7 @@ use constant LANG2COUNTRY => {
     mt => 'MT', # Maltese => Malta
     my => 'MM', # Burmese => Myanmar
     na => 'NR', # Nauru => Nauru
-    nb => 'NO', # Norwegian Bokmål => Norway
+    nb => 'NO', # Norwegian BokmÃ¥l => Norway
     nd => 'ZA', # Ndebele, North => South Africa
     ne => 'NP', # Nepali => Nepal
     # ng => '??', # Ndonga => ??
@@ -197,7 +197,7 @@ use constant LANG2COUNTRY => {
     ur => 'PK', # Urdu => Pakistan
     uz => 'UZ', # Uzbek => Uzbekistan
     vi => 'VN', # Vietnamese => Vietnam
-    # vo => '??', # Volapük => Nowhere
+    # vo => '??', # VolapÃ¼k => Nowhere
     # wo => '??', # Wolof => ??
     xh => 'ZA', # Xhosa => South Africa
     yi => 'IL', # Yiddish => Israel (FIXME: Rather United States?)
@@ -205,6 +205,420 @@ use constant LANG2COUNTRY => {
     # za => '??', # Zhuang => ??
     zh => 'CN', # Chinese => China
     zu => 'ZA', # Zulu => South Africa
+};
+
+# Source: http://msdn.microsoft.com/library/default.asp?url=/library/en-us/vclib/html/_crt_language_strings.asp
+use constant WIN32LANGUAGE => {
+    aa => "Afar",
+    ab => "Abkhazian",
+    ae => "Avestan",
+    af => "Afrikaans",
+    am => "Amharic",
+    ar => "Arabic",
+    as => "Assamese",
+    ay => "Aymara",
+    az => "Azerbaijani",
+    ba => "Bashkir",
+    be => "Belarusian",
+    bg => "Bulgarian",
+    bh => "Bihari",
+    bi => "Bislama",
+    bn => "Bengali",
+    bo => "Tibetan",
+    br => "Breton",
+    bs => "Bosnian",
+    ca => "Catalan",
+    ce => "Chechen",
+    ch => "Chamorro",
+    co => "Corsican",
+    cs => "Czech",
+    cu => "Church Slavic",
+    cv => "Chuvash",
+    cy => "Welsh",
+    da => "Danish",
+    de => "German",
+    dz => "Dzongkha",
+    el => "Greek",
+    en => "English",
+    eo => "Esperanto",
+    es => "Spanish",
+    et => "Estonian",
+    eu => "Basque",
+    fa => "Persian",
+    fi => "Finnish",
+    fj => "Fijian",
+    fo => "Faeroese",
+    fr => "French",
+    fy => "Frisian",
+    ga => "Irish",
+    gd => "Gaelic (Scots)",
+    gl => "Gallegan",
+    gn => "Guarani",
+    gu => "Gujarati",
+    gv => "Manx",
+    ha => "Hausa",
+    he => "Hebrew",
+    hi => "Hindi",
+    ho => "Hiri Motu",
+    hr => "Croatian",
+    hu => "Hungarian",
+    hy => "Armenian",
+    hz => "Herero",
+    ia => "Interlingua",
+    id => "Indonesian",
+    ie => "Interlingue",
+    ik => "Inupiaq",
+    is => "Icelandic",
+    it => "Italian",
+    iu => "Inuktitut",
+    ja => "Japanese",
+    jw => "Javanese",
+    ka => "Georgian",
+    ki => "Kikuyu",
+    kj => "Kuanyama",
+    kk => "Kazakh",
+    kl => "Kalaallisut",
+    km => "Khmer",
+    kn => "Kannada",
+    ko => "Korean",
+    ks => "Kashmiri",
+    ku => "Kurdish",
+    kv => "Komi",
+    kw => "Cornish",
+    ky => "Kirghiz",
+    la => "Latin",
+    lb => "Letzeburgesch",
+    ln => "Lingala",
+    lo => "Lao",
+    lt => "Lithuanian",
+    lv => "Latvian",
+    mg => "Malagasy",
+    mh => "Marshall",
+    mi => "Maori",
+    # Sorry, lads, but that is what M$ calls your language ...
+    mk => "FYRO Macedonian",
+    ml => "Malayalam",
+    mn => "Mongolian",
+    mo => "Moldavian",
+    mr => "Marathi",
+    ms => "Malay",
+    mt => "Maltese",
+    my => "Burmese",
+    na => "Nauru",
+    nb => "Norwegian (BokmÃ¥l)",
+    nd => "Ndebele, North",
+    ne => "Nepali",
+    ng => "Ndonga",
+    nl => "Dutch",
+    nn => "Norwegian-Nynorsk",
+    no => "Norwegian-Nynorsk",
+    nr => "Ndebele, South",
+    nv => "Navajo",
+    ny => "Chichewa",
+    oc => "Occitan (post 1500)",
+    om => "Oromo",
+    or => "Oriya",
+    os => "Ossetian",
+    pa => "Panjabi",
+    pi => "Pali",
+    pl => "Polish",
+    ps => "Pushto",
+    pt => "Portuguese",
+    qu => "Quechua",
+    rm => "Rhaeto-Romance",
+    rn => "Rundi",
+    ro => "Romanian",
+    ru => "Russian",
+    rw => "Kinyarwanda",
+    sa => "Sanskrit",
+    sc => "Sardinian",
+    sd => "Sindhi",
+    se => "Sami",
+    sg => "Sango",
+    si => "Sinhalese",
+    sk => "Slovak",
+    sl => "Slovenian",
+    sm => "Samoan",
+    sn => "Shona",
+    so => "Somali",
+    sq => "Albanian",
+    sr => "Serbian",
+    ss => "Swati",
+    st => "Sotho",
+    su => "Sundanese",
+    sv => "Swedish",
+    sw => "Swahili",
+    ta => "Tamil",
+    te => "Telugu",
+    tg => "Tajik",
+    th => "Thai",
+    ti => "Tigrinya",
+    tk => "Turkmen",
+    tl => "Tagalog",
+    tn => "Tswana",
+    to => "Tonga",
+    tr => "Turkish",
+    ts => "Tsonga",
+    tt => "Tatar",
+    tw => "Twi",
+    ug => "Uighur",
+    uk => "Ukrainian",
+    ur => "Urdu",
+    uz => "Uzbek",
+    vi => "Vietnamese",
+    vo => "Volapuk",
+    wo => "Wolof",
+    xh => "Xhosa",
+    yi => "Yiddish",
+    yo => "Yoruba",
+    za => "Zhuang",
+    zh => "Chinese",
+    zu => "Zulu",
+};
+
+# Source: http://msdn.microsoft.com/library/default.asp?url=/library/en-us/vclib/html/_crt_country_strings.asp
+use constant WIN32COUNTRY => {
+    ad => "Andorra",
+    ae => "United Arab Emirates",
+    af => "Afghanistan",
+    ag => "Antigua and Barbuda",
+    ai => "Anguilla",
+    al => "Albania",
+    am => "Armenia",
+    an => "Netherlands Antilles",
+    ao => "Angola",
+    aq => "Antarctica",
+    ar => "Argentina",
+    as => "American Samoa",
+    at => "Austria",
+    au => "Australia",
+    aw => "Aruba",
+    ax => "Aland Islands",
+    az => "Azerbaijan",
+    ba => "Bosnia and Herzegovina",
+    bb => "Barbados",
+    bd => "Bangladesh",
+    be => "Belgium",
+    bf => "Burkina Faso",
+    bg => "Bulgaria",
+    bh => "Bahrain",
+    bi => "Burundi",
+    bj => "Benin",
+    bm => "Bermuda",
+    bn => "Brunei Darussalam",
+    bo => "Bolivia",
+    br => "Brazil",
+    bs => "Bahamas",
+    bt => "Bhutan",
+    bv => "Bouvet Island",
+    bw => "Botswana",
+    by => "Belarus",
+    bz => "Belize",
+    ca => "Canada",
+    cc => "Cocos (Keeling) Islands",
+    cd => "Congo, The Democratic Republic of the",
+    cf => "Central African Republic",
+    cg => "Congo",
+    ch => "Switzerland",
+    ci => "Cote D'Ivoire",
+    ck => "Cook Islands",
+    cl => "Chile",
+    cm => "Cameroon",
+    cn => "China",
+    co => "Colombia",
+    cr => "Costa Rica",
+    cs => "Serbia and Montenegro",
+    cu => "Cuba",
+    cv => "Cape Verde",
+    cx => "Christmas Island",
+    cy => "Cyprus",
+    cz => "Czech Republic",
+    de => "Germany",
+    dj => "Djibouti",
+    dk => "Denmark",
+    dm => "Dominica",
+    do => "Dominican Republic",
+    dz => "Algeria",
+    ec => "Ecuador",
+    ee => "Estonia",
+    eg => "Egypt",
+    eh => "Western Sahara",
+    er => "Eritrea",
+    es => "Spain",
+    et => "Ethiopia",
+    fi => "Finland",
+    fj => "Fiji",
+    fk => "Falkland Islands (Malvinas)",
+    fm => "Micronesia, Federated States of",
+    fo => "Faroe Islands",
+    fr => "France",
+    fx => "France, Metropolitan",
+    ga => "Gabon",
+    gb => "United Kingdom",
+    gd => "Grenada",
+    ge => "Georgia",
+    gf => "French Guiana",
+    gh => "Ghana",
+    gi => "Gibraltar",
+    gl => "Greenland",
+    gm => "Gambia",
+    gn => "Guinea",
+    gp => "Guadeloupe",
+    gq => "Equatorial Guinea",
+    gr => "Greece",
+    gs => "South Georgia and the South Sandwich Islands",
+    gt => "Guatemala",
+    gu => "Guam",
+    gw => "Guinea-Bissau",
+    gy => "Guyana",
+    hk => "Hong Kong",
+    hm => "Heard Island and McDonald Islands",
+    hn => "Honduras",
+    hr => "Croatia",
+    ht => "Haiti",
+    hu => "Hungary",
+    id => "Indonesia",
+    ie => "Ireland",
+    il => "Israel",
+    in => "India",
+    io => "British Indian Ocean Territory",
+    iq => "Iraq",
+    ir => "Iran",
+    is => "Iceland",
+    it => "Italy",
+    jm => "Jamaica",
+    jo => "Jordan",
+    jp => "Japan",
+    ke => "Kenya",
+    kg => "Kyrgyzstan",
+    kh => "Cambodia",
+    ki => "Kiribati",
+    km => "Comoros",
+    kn => "Saint Kitts and Nevis",
+    kp => "North-Korea",
+    kr => "Korea",
+    kw => "Kuwait",
+    ky => "Cayman Islands",
+    kz => "Kazakhstan",
+    la => "Laos",
+    lb => "Lebanon",
+    lc => "Saint Lucia",
+    li => "Liechtenstein",
+    lk => "Sri Lanka",
+    lr => "Liberia",
+    ls => "Lesotho",
+    lt => "Lithuania",
+    lu => "Luxembourg",
+    lv => "Latvia",
+    ly => "Libyan",
+    ma => "Morocco",
+    mc => "Monaco",
+    md => "Moldova",
+    mg => "Madagascar",
+    mh => "Marshall Islands",
+    mk => "Former Yugoslav Republic of Macedonia",
+    ml => "Mali",
+    mm => "Myanmar",
+    mn => "Mongolia",
+    mo => "Macao",
+    mp => "Northern Mariana Islands",
+    mq => "Martinique",
+    mr => "Mauritania",
+    ms => "Montserrat",
+    mt => "Malta",
+    mu => "Mauritius",
+    mv => "Maldives",
+    mw => "Malawi",
+    mx => "Mexico",
+    my => "Malaysia",
+    mz => "Mozambique",
+    na => "Namibia",
+    nc => "New Caledonia",
+    ne => "Niger",
+    nf => "Norfolk Island",
+    ng => "Nigeria",
+    ni => "Nicaragua",
+    nl => "Netherlands",
+    no => "Norway",
+    np => "Nepal",
+    nr => "Nauru",
+    nu => "Niue",
+    nz => "New Zealand",
+    om => "Oman",
+    pa => "Panama",
+    pe => "Peru",
+    pf => "French Polynesia",
+    pg => "Papua New Guinea",
+    ph => "Philippines",
+    pk => "Pakistan",
+    pl => "Poland",
+    pm => "Saint Pierre and Miquelon",
+    pn => "Pitcairn",
+    pr => "Puerto Rico",
+    ps => "Palestinian Territory, Occupied",
+    pt => "Portugal",
+    pw => "Palau",
+    py => "Paraguay",
+    qa => "Qatar",
+    re => "Reunion",
+    ro => "Romania",
+    ru => "Russian Federation",
+    rw => "Rwanda",
+    sa => "Saudi Arabia",
+    sb => "Solomon Islands",
+    sc => "Seychelles",
+    sd => "Sudan",
+    se => "Sweden",
+    sg => "Singapore",
+    sh => "Saint Helena",
+    si => "Slovenia",
+    sj => "Svalbard and Jan Mayen",
+    sk => "Slovakia",
+    sl => "Sierra Leone",
+    sm => "San Marino",
+    sn => "Senegal",
+    so => "Somalia",
+    sr => "Suriname",
+    st => "Sao Tome and Principe",
+    sv => "El Salvador",
+    sy => "Syrian Arab Republic",
+    sz => "Swaziland",
+    tc => "Turks and Caicos Islands",
+    td => "Chad",
+    tf => "French Southern Territories",
+    tg => "Togo",
+    th => "Thailand",
+    tj => "Tajikistan",
+    tk => "Tokelau",
+    tl => "Timor-Leste",
+    tm => "Turkmenistan",
+    tn => "Tunisia",
+    to => "Tonga",
+    tr => "Turkey",
+    tt => "Trinidad and Tobago",
+    tv => "Tuvalu",
+    tw => "Taiwan, Province of China",
+    tz => "Tanzania, United Republic of",
+    ua => "Ukraine",
+    ug => "Uganda",
+    um => "United States Minor Outlying Islands",
+    us => "United States",
+    uy => "Uruguay",
+    uz => "Uzbekistan",
+    va => "Holy See (Vatican City State)",
+    vc => "Saint Vincent and the Grenadines",
+    ve => "Venezuela",
+    vg => "Virgin Islands, British",
+    vi => "Virgin Islands, U.S.",
+    vn => "Vietnam",
+    vu => "Vanuatu",
+    wf => "Wallis and Futuna",
+    ws => "Samoa",
+    ye => "Yemen",
+    yt => "Mayotte",
+    za => "South Africa",
+    zm => "Zambia",
+    zw => "Zimbabwe",
 };
 
 my $locale_cache;
@@ -280,7 +694,6 @@ sub set_locale {
 	$locale_cache->{$language}->{$country}->{$charset} = undef;
 
 	my $windows = ($^O !~ /darwin/i && $^O =~ /win/i) ? 1 : 0;
-$windows = 1;
 	if ($windows) {
 		return &__set_locale_windows;
 	}
@@ -359,13 +772,11 @@ $windows = 1;
 sub __set_locale_windows {
 	my ($category, $language, $country, $charset) = @_;
 
-	require Locale::Language;
-	require Locale::Country;
-
+    my $set_locale;
 	my $set_language;
 	
 	# First we try to only use the language.
-	my $long_language = Locale::Language::code2language (lc $language);
+	my $long_language = WIN32LANGUAGE->{lc $language};
 	my @languages = ($long_language, $language);
 	my %seen = ();
 	foreach my $language (@languages) {
@@ -379,26 +790,35 @@ sub __set_locale_windows {
 	}
 	
 	# Now try it with the country appended.
- COMBI: foreach my $language (@languages) {
-	    my @short_countries = ($country, LANG2COUNTRY->{lc $language});
-		my @countries = map { 
-			Locale::Country::code2country ($_ } 
-			} grep { defined $_ } @short_countries;
-		foreach my $country (@countries) {
-			next unless defined $country && length $country;
-			my $try = $language . '_' . $country;
-			next if $seen{$try}++;
-			warn "Trying setlocale '$try'.\n" if DEBUG;
-			my $result = POSIX::setlocale ($category, $try);
-			if ($result) {
-				$set_locale = $result;
-				last COMBI;
-			}
-		}
+	if (defined $country) {
+        COMBI: foreach my $language (@languages) {
+            # We do not need a fallback country here, because the "system" already
+            # provides the information.
+	        my @short_countries = ($country);
+		    my @countries = map { 
+			    WIN32COUNTRY->{lc $_} 
+			    } grep { defined $_ } @short_countries;
+		    foreach my $country (@countries) {
+			    next unless defined $country && length $country;
+			    my $try = $language . '_' . $country;
+			    next if $seen{$try}++;
+			    warn "Trying setlocale '$try'.\n" if DEBUG;
+			    my $result = POSIX::setlocale ($category, $try);
+			    if ($result) {
+				    $set_locale = $result;
+				    last COMBI;
+			    }
+		    }
+	    }
 	}
 
+	return unless defined $set_locale && length $set_locale;
 
-	die;
+    # Apparently, there is no point in setting a charset.  Even the new
+    # MS-DOS versions like 2000 or XP still have the concept of more or
+    # less fixed codepages.  Switching to UTF-8 does not work.
+	$locale_cache->{$language}->{$country}->{$charset} = $set_locale;
+	return $set_locale;
 }
 
 1;
