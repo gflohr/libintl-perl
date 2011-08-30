10cc61e0d4ae48931ae9c0f131a04a3bafa1b276
Guido Flohr <guido.flohr@cantanea.com>
2011-08-30 14:59:24 +0300

    Base for MO class

---
+++ lib/Locale/Catalog/Format/MO.pm
@@ -0,0 +1,54 @@
+#! /bin/false
+
+# vim: set autoindent shiftwidth=4 tabstop=4:
+
+# This file is part of libintl-perl.
+# Copyright (C) 2002-2011 Guido Flohr <guido@imperia.net>,
+# all rights reserved.
+
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Library General Public License as published
+# by the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+                                         
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+
+# You should have received a copy of the GNU Library General Public 
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, 
+# USA.
+
+package Locale::Catalog::Format::MO;
+
+use base qw (Locale::Catalog);
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+Locale::Catalog::Format::MO - Compiled binary message catalogs
+
+=head1 SYNOPSIS
+
+  use Locale::Catalog::Format::MO;
+
+=head1 DESCRIPTION
+
+The module Locale::Catalog::Format::MO(3pm) represents a binary, compiled 
+message catalog, commonly known as MO files.
+
+The format is descibed at 
+L<http://www.gnu.org/s/hello/manual/gettext/MO-Files.html>.
+
+The module is a subclass of Locale::Catalog(3pm).  See that documentation 
+for details.
+
+=head1 SEE ALSO
+
+Locale::Catalog(3pm), perl(1)
---
+++ tests/06mo-write.t
@@ -0,0 +1,19 @@
+#! /usr/local/bin/perl -w
+
+# vim: syntax=perl
+# vim: tabstop=4
+
+use strict;
+
+use Test;
+
+use constant NUM_TESTS => 3;
+
+use Locale::Catalog::Format::MO;
+
+BEGIN {
+	plan tests => NUM_TESTS;
+}
+
+my $catalog = Locale::Catalog::Format::MO->new;
+ok defined $catalog;
