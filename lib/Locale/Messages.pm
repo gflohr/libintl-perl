2e9b2284883b84eab39cc45eb864d6688ba2f930
Guido Flohr <guido.flohr@cantanea.com>
2017-08-31 21:56:12 +0300

    bumped version number to 1.28

--- lib/Locale/Messages.pm
+++ lib/Locale/Messages.pm
@@ -26,7 +26,7 @@ use File::Spec;
 
 use vars qw ($package @EXPORT_OK %EXPORT_TAGS @ISA $VERSION);
 
-$VERSION = '1.27';
+$VERSION = '1.28';
 
 # Try to load the C version first.
 $package = 'gettext_xs';
--- lib/Locale/TextDomain.pm
+++ lib/Locale/TextDomain.pm
@@ -66,7 +66,7 @@ use Cwd qw (abs_path);
 
 use vars qw ($VERSION);
 
-$VERSION = '1.27';
+$VERSION = '1.28';
 
 require Exporter;
 
